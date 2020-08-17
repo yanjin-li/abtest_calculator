ba19de12721539c6aa00195dfe9fc5e9a51d04f9
yanjin-li <66980007+yanjin-li@users.noreply.github.com>
2020-08-17 16:36:20 +0900

    finished the V1 for proportion test sample size calculator part

--- ui.R
+++ ui.R
@@ -1,57 +1,235 @@
-library(shiny)
-library(shinythemes)
+# Shiny Initializations --------------------------------------------------------
 
-ui <- shinyUI(fluidPage(
-  
-  theme = shinytheme("united"),
-  # title and text
-  fluidRow(align = "center",
-           titlePanel("Sample Size for your A/B Test"),
-           column(10, offset = 1,
-                  p("This shiny app allows you to calculate the minimum sample
-          size for your A/B test.  The plot shows you the hypothetical
-          probability density for the test along with errors.  Use the
-          controller to select the parameters for your test and hit the",
-                    code("submit"),
-                    "button to calculate the sample size.  All values in the
-          controller are as a percent.  (note that if the sample size is very
-          large, it may take ggplot a second to render)")
-           )
-  ),
-  # horizontal ruler
-  tags$hr(),
-  # density plot
-  plotOutput("main_plot"),
-  # horizontal ruler
-  tags$hr(),
-  # sample size output
-  fluidRow(align = "center",
-           h4("sample size:"),
-           h1(textOutput("pred_out"))
-  ),
-  # horizontal ruler
-  tags$hr(),
-  # selector/controller for sample size and plot
-  fluidRow(
-    column(4,
-           numericInput("numericP", "current baseline conversion rate",
-                        value = 15, min = 1, max = 98, step = 0.5),
-           numericInput("numericDmin", "desired minimum detectable change",
-                        value = 2, min = 0.5, max = 20, step = 0.5)
+# load packages 
+pacman::p_load(
+  shinydashboard,
+  shiny, dplyr, here, 
+  glue, lubridate
+)
+
+
+# sidebar 
+sidebar <- dashboardSidebar(
+  sidebarMenu(
+    menuItem(
+      "Proportion Test (CTR, etc.)", 
+      tabName = "prop_test", 
+      icon = icon("percent")
     ),
-    column(4,
-           sliderInput("sliderAlpha", "significance level",
-                       value = 5, min = 1, max = 20, step = 0.5),
-           sliderInput("sliderPower", "statistical power",
-                       value = 80, min = 50, max = 98, step = 0.5)
-    ),
-    column(4,
-           radioButtons("type", "Test for change in:",
-                        c("either direction" = "two",
-                          "one direction - less" = "less",
-                          "one direction - greater" = "greater")),
-           submitButton("submit")
+    menuItem(
+      "t-Test (FQ28, etc.)", 
+      tabName = "t_test", 
+      icon = icon("hourglass-half")
     )
   )
+)
+
+body <- dashboardBody(
   
-))
+  tabItems(
+    
+    tabItem(
+      tabName = "prop_test",
+      
+      fluidRow(
+        # 1. Baseline CVR and Lift %  
+        box(
+          "Step 1: Enter the baseline conversion rate for the page, experience, and audience 
+        you hope to impact (e.g., enter “5” for “5%”):",
+          numericInput(
+            inputId = "prop_test_baseline", 
+            label = "Baseline converstion rate (%):",
+            value = 15.0
+          ),
+          "Step 2: Enter the estimated lift percentage you would need to see in order to make 
+        a decision (positive or negative):",
+          numericInput(
+            inputId = "prop_test_min_effect", 
+            label = "Expected lift percentage (%):",
+            value = 1.0
+          ), 
+          title = "1. Baseline Conversion Rate and Estimated Life %", 
+          solidHeader = FALSE,
+          status = "primary",
+          height = 300
+        ),
+        
+        # 2. Traffic Volume and # of Variations
+        box(
+          "How many visit(or)s do you get to the experience, on average, over the 
+            course of 30 days?",
+          numericInput(
+            inputId = "prop_test_traffic", 
+            label = "Traffic (Two Weeks User Count):",
+            value = 10000
+          ),
+          "How many total variations are you testing, including the control (e.g.,
+            a standard A/B test would be “2”)?",
+          numericInput(
+            inputId = "prop_test_variations", 
+            label = "Number of Variations:",
+            value = 2
+          ), 
+          title = "2. Traffic Volume and # of Variations (Groups)", 
+          solidHeader = FALSE,
+          status = "primary",
+          height = 300
+        )
+      ),
+      
+      
+      fluidRow(
+        # 3. Tail Options 
+        box(
+          "Is your hypothesis directional (e.g., you expect the conversion rate 
+            to increase) or non-directional (e.g., you expect the challenger to 
+            be different, but you have no evidence to support the challenger as 
+            better)?", br(), 
+          selectInput(
+            inputId = "prop_test_tail_option", 
+            label = "Test Tail Option:",
+            choices = c(
+              "Two-side (A non-directional hypothesis: B = A)" = "two.sided",
+              "One-side (A directional hypothesis: B > or < A)" = "one.sided"
+              )
+          ),
+          #"Note: Results within your testing tool may vary if you select one-tailed 
+          #  and your testing tool uses two-tailed and vice versa.",
+          title = "3. Select 1-Tailed vs. 2-Tailed", 
+          solidHeader = FALSE,
+          status = "primary",
+          width = 4,
+          height = 230
+        ),
+        
+        # 4. Alpha 
+        box(
+          "How important is it that you do not erroneously report a difference 
+          when, in reality, the variations are the same?", br(), 
+          sliderInput(
+            inputId = "prop_test_alpha", 
+            label = div(style='width:350px;',
+                        div(style='float:left;', 'Not Important'),
+                        div(style='float:right;', 'Very Important')),
+            min = 50,
+            max = 99,
+            value = 95,
+            post = "%"
+          ),
+          #"This is the statistical confidence. The higher you set the statistical 
+          #confidence, the less likely the statistical results will return a false 
+          #difference (aka, a false positive or a Type I error).", 
+          title = "4. Select Confidence (1-\u03B1)", 
+          solidHeader = FALSE,
+          status = "primary",
+          width = 4,
+          height = 230
+        ),
+        
+        
+        # 5. Beta  
+        box(
+          "How important is it that you do not erroneously report NO difference 
+          when, in reality, there is a difference between the variations?", br(), 
+          sliderInput(
+            inputId = "prop_test_beta", 
+            label = div(style='width:350px;',
+                        div(style='float:left;', 'Not Important'),
+                        div(style='float:right;', 'Very Important')),
+            min = 50,
+            max = 99,
+            value = 80,
+            post = "%"
+          ),
+          #"The higher you set the statistical power, the greater your likelihood 
+          #of detecting a real difference if one exists and the less likely you 
+          #will return a false “no difference” (aka, a false negative or a Type 
+          #II error).", 
+          title = "4. Select Confidence (1-\u03B2)", 
+          solidHeader = FALSE,
+          status = "primary",
+          width = 4,
+          height = 230
+        )
+      ),
+      
+      fluidRow(
+        box(
+          "The number of visit(or)s should be at least:",
+          h1(
+            style = "text-align: center;", 
+            textOutput("propTestSampleSizeBox", inline=TRUE), 
+            span(style="display: inline-block; font-size:40px;", "per variation")
+            ),
+          h2(
+            style = "text-align: center;", 
+            textOutput("propTestTotalVisitors", inline=TRUE)
+            ),
+          "This is the number of visitors required to detect a change in the 
+            conversion rate from the baseline (%) to (1+lift) x basline (%).",
+          title = "Minimum Sample Size", 
+          solidHeader = FALSE,
+          status = "success",
+          width = 12,
+          height = 230
+        )
+      )
+  ),
+  
+  tabItem(
+      tabName = "t_test"
+    )
+  ) 
+)
+
+# UI
+ui <- dashboardPage(
+  skin = "black",
+  dashboardHeader(title = "A/B Test Sample Size"),
+  sidebar,
+  body
+)
+
+
+
+
+
+server <- function(input, output) {
+  # Reactive function to do the actual sample size calculation. This has error
+  # checking baked in. And, it returns a number or a string
+  calc_results <- reactive({
+    tryCatch(
+      {
+        # Calculate the result
+        calc_n <- power.prop.test(p1 = input$prop_test_baseline/100,
+                                  p2 = (input$prop_test_baseline/100) * (1 + input$prop_test_min_effect/100),
+                                  sig.level = (100 - input$prop_test_alpha)/100,
+                                  power = input$prop_test_beta/100,
+                                  alternative = input$prop_test_tail_option)
+        
+        # Extract the sample size from the returned list and return it
+        return(calc_n$n)
+      }#,
+      # error = function(e){
+      #   return("N/A")
+      # }
+    )
+  })
+  
+  output$propTestSampleSizeBox <- renderText({
+    result <- round(calc_results())
+    format(result, big.mark=",", scientific = FALSE)
+    
+  })
+  
+  output$propTestTotalVisitors <- renderText({
+    result <- round(calc_results()*input$prop_test_variations)
+     paste0("(",format(result, big.mark=",", scientific = FALSE),
+                         " total visitors)")
+  })
+  
+}
+
+
+# Run the App ------------------------------------------------------------------
+shinyApp(ui, server)
